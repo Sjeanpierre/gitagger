caea30df6aae398852648f8a1644d635a598463b
stevenson.jean-pierre@sage.com <stevenson.jean-pierre@sage.com>
2013-01-30 18:51:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitagger::Application.config.secret_token = '17c38f64f6228d4f9470db6a11f18de1dbb827a38af80489d7573ad187613c80955a270a0317c01c4f4267362ee160ce694644b6eadeddc527cfd9f2fe893b73'
