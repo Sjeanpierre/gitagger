72553ab3baaa8046465e995e81a339faca529878
stevenson.jean-pierre@sage.com <stevenson.jean-pierre@sage.com>
2013-02-03 02:35:49 -0500

    Returning list of all user repositories

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :git_token
 	has_many :authorizations
 	validates :name, :email, :git_token, :presence => true
+
+	def get_current_user
+		@user = User.find(session[:user_id])
+	end
 end
