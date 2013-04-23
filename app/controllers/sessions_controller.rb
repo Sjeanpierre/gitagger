9f0f3b09263a8351baccc83919fb4ec33f740630
stevenson.jean-pierre@sage.com <stevenson.jean-pierre@sage.com>
2013-04-23 14:22:31 -0700

    moved sessions to DB to handle issue #13

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,25 +4,28 @@ class SessionsController < ApplicationController
 
   def create
 		authentication_hash = request.env['omniauth.auth']
-		@authorization = Authorization.find_by_provider_and_uid(authentication_hash["provider"], authentication_hash["uid"])
+		@authorization = Authorization.find_by_provider_and_uid(authentication_hash['provider'], authentication_hash['uid'])
 		if @authorization
 			session[:user_id] = @authorization.user.id
 			#redirect user to repo list once logged in
-			render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
+			flash[:notice] = "Welcome back #{@authorization.user.name}!"
+			redirect_to :controller => 'github', :action => 'repo'
 		else
-			user = User.new(:name => authentication_hash["info"]["name"],
-											:email => authentication_hash["info"]["email"],
-											:git_token => authentication_hash["credentials"]["token"]
+			user = User.new(:name => authentication_hash['info']['name'],
+											:email => authentication_hash['info']['email'],
+											:git_token => authentication_hash['credentials']['token']
 											)
-			user.authorizations.build :provider => authentication_hash["provider"], :uid => authentication_hash["uid"]
+			user.authorizations.build :provider => authentication_hash['provider'], :uid => authentication_hash['uid']
 			user.save
 			session[:user_id] = user.id
 			#redirect user to repo list when signup is complete
-			render :text => "Hi #{user.name}! You've signed up."
+			flash[:notice] = "Thank you for signing up #{user.name}!"
+			redirect_to :controller => 'github', :action => 'repo'
 		end
 	end
 
 	def destroy
+		# is there a better way to destroy sessions?
 		session[:user_id] = nil
 		render :text => "You've logged out!"
 	end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Gitagger::Application.config.session_store :cookie_store, key: '_gitagger_session'
+#Gitagger::Application.config.session_store :cookie_store, key: '_gitagger_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Gitagger::Application.config.session_store :active_record_store
+Gitagger::Application.config.session_store :active_record_store
