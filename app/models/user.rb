90c9db9a8c863c1656b2ae1458b7987fead95179
stevenson.jean-pierre@sage.com <stevenson.jean-pierre@sage.com>
2013-04-23 14:19:21 -0700

    addresses issue #3

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
 	def get_current_user
 		@user = User.find(session[:user_id])
 	end
+
+	def self.valid_session?(session)
+		User.exists?(session[:user_id])
+	end
+
+	def self.handle_revoked_token(session)
+		user = User.find(session[:user_id])
+		user.authorizations.destroy_all
+	end
+
 end
